ff82697308e51cab2b97f53d7b91d1992bff1f11
Olof Morra <olofmorra@gmail.com>
2019-03-11 17:41:42 +0100

    Created the start of the input panel with some basic CSS and the first input possibilities. Possibility to choose file and split type for reading data. The actual reading of the data is not yet working.

--- app.py
+++ app.py
@@ -3,121 +3,90 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+
 import plotly.plotly as py
 import plotly.graph_objs as go
 
+import pandas as pd
+import base64
+import datetime
+import io
+
 import networkx as nx
-
-G=nx.random_geometric_graph(1000,0.025)
-pos=nx.get_node_attributes(G,'pos')
-
-dmin=1
-ncenter=0
-for n in pos:
-    x,y=pos[n]
-    d=(x-0.5)**2+(y-0.5)**2
-    if d<dmin:
-        ncenter=n
-        dmin=d
-
-p=nx.single_source_shortest_path_length(G,ncenter)
-
-edge_trace = go.Scatter(
-    x=[],
-    y=[],
-    line=dict(width=0.5,color='#888'),
-    hoverinfo='none',
-    mode='lines')
-
-for edge in G.edges():
-    x0, y0 = G.node[edge[0]]['pos']
-    x1, y1 = G.node[edge[1]]['pos']
-    edge_trace['x'] += tuple([x0, x1, None])
-    edge_trace['y'] += tuple([y0, y1, None])
-
-node_trace = go.Scatter(
-    x=[],
-    y=[],
-    text=[],
-    mode='markers',
-    hoverinfo='text',
-    marker=dict(
-        showscale=True,
-        # colorscale options
-        #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-        #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-        #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-        colorscale='YlGnBu',
-        reversescale=True,
-        color=[],
-        size=10,
-        colorbar=dict(
-            thickness=15,
-            title='Node Connections',
-            xanchor='left',
-            titleside='right'
-        ),
-        line=dict(width=2)))
-
-for node in G.nodes():
-    x, y = G.node[node]['pos']
-    node_trace['x'] += tuple([x])
-    node_trace['y'] += tuple([y])
-
-for node, adjacencies in enumerate(G.adjacency()):
-    node_trace['marker']['color']+=tuple([len(adjacencies[1])])
-    node_info = '# of connections: '+str(len(adjacencies[1]))
-    node_trace['text']+=tuple([node_info])
-
-fig = {'data':[edge_trace, node_trace],
-             'layout':go.Layout(
-                title='<br>Network graph made with Python',
-                titlefont=dict(size=16),
-                showlegend=False,
-                hovermode='closest',
-                margin=dict(b=20,l=5,r=5,t=40),
-                annotations=[ dict(
-                    text="Python code: <a href='https://plot.ly/ipython-notebooks/network-graphs/'> https://plot.ly/ipython-notebooks/network-graphs/</a>",
-                    showarrow=False,
-                    xref="paper", yref="paper",
-                    x=0.005, y=-0.002 ) ],
-                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))}
-
-graphs = []
-for i in range(1,3):
-    graph = dcc.Graph(
-        id='example-graph{}'.format(i),
-        figure=fig
-    )
-    graphs.append(graph)
-
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(className='main-body', children=[
-    html.Div(className='main-sidebar', children=['Input panel',
-        dcc.Dropdown(id="my-id",
-            options=[
-                {'label': 'Dijkstra', 'value': 'Dijkstra'},
-                {'label': 'Bellman-Ford', 'value': 'Bellman-Ford'},
-                {'label': 'A*', 'value': 'A*'}
-            ],
-            value='A*'
-        ),
-        html.Div(id='entry')]),
+df = None
 
-    html.Div(className='main-content',
-        children= graphs),
-    html.Div(className='main-sidebar', children=['Right panel'])
+app.layout = html.Div(id='main-body', children=[
+    html.Div(className='input-panel', children=[
+        html.H1('Input panel'),
+        html.P('Select file type'),
+        dcc.Dropdown(id="dropdown-filetype",
+            options=[
+                {'label': 'CSV', 'value': 'csv'},
+                {'label': 'XLS', 'value': 'xls'}
+            ],
+            value='csv'
+        ),
+        html.P('Select split character'),
+        dcc.Dropdown(id="dropdown-splittype",
+            options=[
+                {'label': 'Space', 'value': ' '},
+                {'label': ',', 'value': ','},
+                {'label': "Tab", 'value': '\t'}
+            ],
+            value=' '
+        ),
+        dcc.Upload(
+            id='upload-field',
+            className='upload-data',
+            children=html.Div([
+                'Drag and Drop or ',
+                html.A('Select Files')
+            ]),
+            # Do not allow multiple files to be uploaded
+            multiple=False
+        ),
+        html.Div(id='head-data-upload')]
+    ),
+    html.Div(className='vis-panel',
+        children=[]
+    ),
+    html.Div(className='output-panel',
+             children=['Right panel']
+    )
 ])
 
-@app.callback(
-    Output(component_id='entry', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+
+# Callback functions; functions that execute when something is changed
+@app.callback(Output('head-data-upload', 'children'),
+              [Input('upload-field', 'contents'),
+               Input('dropdown-splittype', 'value'),
+               Input('dropdown-filetype', 'value')],
+              )
+def load_data(content, split, filetype):
+    if (content == None):
+        return html.Div([
+            'Select a data file.'
+        ])
+    elif (split == None):
+        return html.Div([
+            'Select a split character.'
+        ])
+    elif (filetype == None):
+        return html.Div([
+            'Select a filetype.'
+        ])
+    else:
+        return html.Div([
+            'Loaded' + content['name']
+        ])
+
+    if filetype == 'csv':
+        df = pd.read_csv(content)
+    elif filetype == 'xls':
+        df = pd.read_excel(content)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
