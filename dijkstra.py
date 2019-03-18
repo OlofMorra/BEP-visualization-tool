2d5f9d968733b398c14cdddc93fa58ee966a5690
Freek-R <33570091+Freek-R@users.noreply.github.com>
2019-03-18 17:22:08 +0100

    Dijkstra implemented
    Partially completed docstring added to dijkstra(), which is in accordance with the "Numpy Style" Python docstrings.

--- dijkstra.py
+++ dijkstra.py
@@ -3,8 +3,25 @@ import numpy as np
 import networkx as nx
 from heapq import heapify, heappush, heappop
 
+def dijkstra(G:nx.Graph, source:int, weight:str="weight"):
+    """Implementation of Dijkstra's shortest path algorithm to find shortest weighted paths from source to all other
+    nodes graph G.
 
-def dijkstra(G:nx.Graph, source:int, length:str="length"):
+    Parameters
+    ----------
+    G : nx.Graph
+        Description of arg1
+    source : str
+        Description of arg2
+    length : str
+        Description of arg3
+
+    Returns
+    -------
+    (dict, dict)
+        Description of return value
+    """
+
     Q = list()  # list of (distance, vertex) tuples, maintained as a min heap by distance
     dist = dict()  # key is a vertex and value the distance between source and key
     prev = dict()  # key is a vertex and value the previous vertex on the path from source to key
@@ -23,7 +40,7 @@ def dijkstra(G:nx.Graph, source:int, length:str="length"):
         neighs_u = [x for x in nx.neighbors(G, u) if x in [y[1] for y in Q]]  # neighbors of u that are still in Q
 
         for v in neighs_u:
-            alt = dist[u] + G.edges[u, v][length]  # dist(source, u) + dist(u, v)
+            alt = dist[u] + G.edges[u, v][weight]  # dist(source, u) + dist(u, v)
 
             if alt < dist[v]:  # update dist if alt dist < current dist
                 dist[v] = alt
