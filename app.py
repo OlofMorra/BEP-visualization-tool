9e55044b573db7167a000d66c5da9658c8bc2506
Freek-R <33570091+freek-r@users.noreply.github.com>
2019-05-07 16:36:51 +0200

    Run button, multiple graph display, and network visualization added
    Clicking the button in the input panel adds a graph to the visual analytics panel. There's currently no limit to the number of graphs that can be displayed. As soon as a dataset is selected in the dropdown menu, the right/output panel shows a network visualization of the graph. The layout of the visualization can be changed with a dropdown menu.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_cytoscape as cyto
 from dash.dependencies import Input, Output, State
 import dash_table
 import plotly.plotly as py
@@ -11,6 +12,7 @@ import pandas as pd
 import base64
 import io
 import random
+import json
 
 import networkx as nx
 from algorithms import dijkstra, temp_func
@@ -27,15 +29,16 @@ df2 = pd.DataFrame({'source': [random.randint(1, 10) for _ in range(10)],
                     'target': [random.randint(1, 10) for _ in range(10)],
                     'weight': [random.lognormvariate(mu=0, sigma=0.5) for _ in range(10)]})
 G2 = nx.DiGraph(df2)
-datasets = [df1, df2]
+datasets = {'df1': df1, 'df2': df2}
 
 # structure: {algorithm type: {algorithm: [html structure]}}
 algorithms = {
     'Shortest path': ['Dijkstra', 'Bellman-Ford', 'Floyd-Warshall'],
     'Minimal spanning tree': ['Kruskal', 'Prim'],
-    'Matching': ['Ford-Fulkerson'],
-    'Test': ['temp_func']
+    'Matching': ['Ford-Fulkerson']
 }
+
+network_layouts = ['breadthfirst', 'circle', 'concentric', 'cose', 'grid', 'random']
 
 
 # APP LAYOUT
@@ -68,7 +71,9 @@ app.layout = html.Div(id='main-body', children=[
         html.Hr(),
         html.H1('Algorithm settings'),
         html.P('Select dataset'),
-        dcc.Dropdown(id='dijkstra-graph-dropdown'),
+        dcc.Dropdown(id='dataset-dropdown',
+                     options=[{'label': 'df1', 'value': 'df1'}, {'label': 'df2', 'value': 'df2'}],
+                     value='df1'),
         html.P('Select algorithm type'),
         dcc.Dropdown(
             id="algorithm-type-dropdown",
@@ -86,7 +91,7 @@ app.layout = html.Div(id='main-body', children=[
                 dcc.Dropdown(id='dijkstra-start-dropdown'),
                 html.P('Select weight column'),
                 dcc.Dropdown(id='dijkstra-weight-dropdown'),
-                html.Button(id='dijkstra-run-button', children='Run algorithm')
+                html.Button(id='dijkstra-run-button', n_clicks=0, children='Run algorithm', type='submit')
             ]),
             # Bellman-Ford
             html.Div(id='bellman-ford-settings', style={'display': 'none'}, children=[]),
@@ -97,24 +102,25 @@ app.layout = html.Div(id='main-body', children=[
             # Prim
             html.Div(id='prim-settings', style={'display': 'none'}, children=[]),
             # Ford-Fulkerson
-            html.Div(id='ford-fulkerson-settings', style={'display': 'none'}, children=[]),
-            # Test
-            html.Div(id='test-settings', style={'display': 'none'}, children=[
-                dcc.Input(id='temp_func-func_name-input'),
-                dcc.Dropdown(id='temp_func-start-dropdown')
-            ])
+            html.Div(id='ford-fulkerson-settings', style={'display': 'none'}, children=[])
         ]),
     ]),
 
     # VISUAL ANALYTICS PANEL
     html.Div(className='vis-panel', children=[
-        'Visual analytics panel',
-        html.Div(id='vis-test')
+        html.H1('Visual analytics panel'),
+        html.Div(id='vis-graphs')
     ]),
 
     # OUTPUT PANEL
     html.Div(className='output-panel', children=[
-        'Right panel'
+        html.H1('Right panel'),
+        html.P('Layout:', style={'width': '25%', 'display': 'inline-block'}),
+        dcc.Dropdown(id='network-layout-dropdown',
+                     options=[{'label': x, 'value': x} for x in network_layouts],
+                     value=network_layouts[0],
+                     style={'width': '75%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+        html.Div(id='network')
     ])
 ])
 
@@ -142,8 +148,9 @@ def parse_contents(contents, filetype, filename):
 
     return
 
-
-# CALLBACK FUNCTIONS
+#########################
+# INPUT PANEL CALLBACKS #
+#########################
 @app.callback(Output('head-data-upload', 'children'),
               [Input('upload-field', 'contents'),
                Input('dropdown-filetype', 'value')],
@@ -175,14 +182,15 @@ def set_algorithm_value(available_options):
                Output('floyd-warshall-settings', 'style'),
                Output('kruskal-settings', 'style'),
                Output('prim-settings', 'style'),
-               Output('ford-fulkerson-settings', 'style'),
-               Output('test-settings', 'style')],
+               Output('ford-fulkerson-settings', 'style')],
               [Input('algorithm-dropdown', 'value')])
 def show_settings(selected_algorithm):
     result_dict = {x: {'display': 'none'} for y in algorithms.values() for x in y}  # hide all divs
     result_dict[selected_algorithm] = {}  # remove the selected algorithm's div's style settings
     return [x for x in result_dict.values()]
 
+
+# Dijkstra callbacks
 # TODO account for possible changes in Graph object
 @app.callback(Output('dijkstra-start-dropdown', 'options'),
               [Input('dijkstra-settings', 'style')])
@@ -221,6 +229,46 @@ def set_dijkstra_start_value(options):
     else:
         return ''
 
+
+####################################
+# VISUAL ANALYTICS PANEL CALLBACKS #
+####################################
+@app.callback(Output('vis-graphs', 'children'),
+              [Input('dijkstra-run-button', 'n_clicks')],
+              [State('dijkstra-start-dropdown', 'value'), State('dijkstra-weight-dropdown', 'value'),
+               State('dataset-dropdown', 'value'), State('vis-graphs', 'children')])
+def run_dijkstra(n_clicks, start, weight, df, current_graphs):
+    graph = dcc.Graph(figure={
+        'data': [{'x': datasets[df]['source'], 'y': datasets[df]['target'], 'type': 'line', 'name': 'SF'}]
+    })
+    if n_clicks > 0:
+        if current_graphs is None:
+            return list([graph])
+        else:
+            current_graphs.append(graph)
+            return current_graphs
+
+
+##########################
+# OUTPUT PANEL CALLBACKS #
+##########################
+@app.callback(Output('network', 'children'),
+              [Input('dataset-dropdown', 'value'), Input('network-layout-dropdown', 'value')])
+def show_network(df_name, layout):
+    df = datasets[df_name]
+    set_of_nodes = set(df['source']) | set(df['target'])  # union of the sets of source and target nodes
+    nodes = [{'data': {'id': x, 'label': x}} for x in set_of_nodes]
+    edges = [{'data': {'source': row['source'], 'target': row['target']}} for i, row in df[['source', 'target']].iterrows()]
+    elements = nodes + edges
+
+    return html.H3(df_name), cyto.Cytoscape(
+        id='cytoscape-layout-1',
+        elements=elements,
+        style={'width': '100%', 'height': '350px'},
+        layout={
+            'name': layout
+        }
+    )
 
 if __name__ == '__main__':
     app.run_server(debug=True)
